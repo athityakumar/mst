e12d289929279768491c21290a7380f35d29f586
Athitya Kumar <athityakumar@gmail.com>
2017-04-08 22:02:19 +0530

    Changes logic of max spanning tree

--- mst.py
+++ mst.py
@@ -5,6 +5,8 @@ Arc = namedtuple('Arc', ('tail', 'weight', 'head'))
 
 # The third argument of gen_spanning_arborescence funtion specifies whether the result returned will be a Minimum spanning tree or a Maximum spanning tree.
 def gen_spanning_arborescence(arcs, sink, opt):
+    if opt == "max":
+        arcs = [Arc(arc.tail, arc.weight * (-1), arc.head) for arc in arcs]
     good_arcs = []
     quotient_map = {arc.tail: arc.tail for arc in arcs}
     quotient_map[sink] = sink
@@ -18,13 +20,13 @@ def gen_spanning_arborescence(arcs, sink, opt):
             head_rep = quotient_map[arc.head]
             if tail_rep == head_rep:
                 continue
-            if tail_rep not in gen_arc_by_tail_rep or ((gen_arc_by_tail_rep[tail_rep].weight > arc.weight and opt == "min") or (gen_arc_by_tail_rep[tail_rep].weight < arc.weight and opt == "max")):
+            if tail_rep not in gen_arc_by_tail_rep or gen_arc_by_tail_rep[tail_rep].weight > arc.weight:
                 gen_arc_by_tail_rep[tail_rep] = arc
                 successor_rep[tail_rep] = head_rep
         cycle_reps = find_cycle(successor_rep, sink)
         if cycle_reps is None:
             good_arcs.extend(gen_arc_by_tail_rep.values())
-            return spanning_arborescence(good_arcs, sink)
+            return spanning_arborescence(good_arcs, sink, opt)
         good_arcs.extend(gen_arc_by_tail_rep[cycle_rep] for cycle_rep in cycle_reps)
         cycle_rep_set = set(cycle_reps)
         cycle_rep = cycle_rep_set.pop()
@@ -42,7 +44,7 @@ def find_cycle(successor, sink):
             return cycle[cycle.index(node):]
     return None
 
-def spanning_arborescence(arcs, sink):
+def spanning_arborescence(arcs, sink, opt):
     arcs_by_head = defaultdict(list)
     for arc in arcs:
         if arc.tail == sink:
@@ -54,7 +56,10 @@ def spanning_arborescence(arcs, sink):
         arc = stack.pop()
         if arc.tail in solution_arc_by_tail:
             continue
-        solution_arc_by_tail[arc.tail] = arc
+        if opt == "min":
+            solution_arc_by_tail[arc.tail] = arc
+        if opt == "max":
+            solution_arc_by_tail[arc.tail] = Arc(arc.tail, arc.weight*(-1), arc.head)
         stack.extend(arcs_by_head[arc.tail])
     return solution_arc_by_tail
 
@@ -64,9 +69,14 @@ if __name__ == "__main__":
 
     negative_arcs = [Arc(1, -17, 0), Arc(2, -16, 0), Arc(3, -19, 0), Arc(4, -16, 0), Arc(5, -16, 0), Arc(6, -18, 0), Arc(2, -3, 1), Arc(3, -3, 1), Arc(4, -11, 1), Arc(5, -10, 1), Arc(6, -12, 1), Arc(1, -3, 2), Arc(3, -4, 2), Arc(4, -8, 2), Arc(5, -8, 2), Arc(6, -11, 2), Arc(1, -3, 3), Arc(2, -4, 3), Arc(4, -12, 3), Arc(5, -11, 3), Arc(6, -14, 3), Arc(1, -11, 4), Arc(2, -8, 4), Arc(3, -12, 4), Arc(5, -6, 4), Arc(6, -10, 4), Arc(1, -10, 5), Arc(2, -8, 5), Arc(3, -11, 5), Arc(4, -6, 5), Arc(6, -4, 5), Arc(1, -12, 6), Arc(2, -11, 6), Arc(3, -14, 6), Arc(4, -10, 6), Arc(5, -4, 6)]
 
+
+    test_arcs = [Arc(1, 9, 0), Arc(2, 8, 0), Arc(3, 9, 0), Arc(2, 20, 1), Arc(3, 3, 1), Arc(1,30, 2), Arc(3, 30, 2), Arc(2, 50, 3), Arc(1, 11, 3)]
+
     sink = 0
 
     print("Minimum spanning tree on positive arcs values : \n",gen_spanning_arborescence(positive_arcs, sink, "min"),"\n")
     print("Maximum spanning tree on positive arcs values : \n",gen_spanning_arborescence(positive_arcs, sink, "max"),"\n")
     print("Minimum spanning tree on negative arcs values : \n",gen_spanning_arborescence(negative_arcs, sink, "min"),"\n")
     print("Maximum spanning tree on negative arcs values : \n",gen_spanning_arborescence(negative_arcs, sink, "max"),"\n")
+    print("Test min case : \n",gen_spanning_arborescence(test_arcs, sink, "min"))
+    print("Test max case : \n",gen_spanning_arborescence(test_arcs, sink, "max"))
