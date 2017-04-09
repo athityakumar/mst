21ec99e21ab72d97462784d16496ab4c37c6f8c1
Athitya Kumar <athityakumar@gmail.com>
2017-04-09 15:25:16 +0530

    Adds helper function to find_cycle() before popping

--- mst.py
+++ mst.py
@@ -27,10 +27,28 @@ def gen_spanning_arborescence(arcs, sink, opt):
         if cycle_reps is None:
             good_arcs.extend(gen_arc_by_tail_rep.values())
             return spanning_arborescence(good_arcs, sink, opt)
+        cycle_reps = kind_of_sort_by_weights(arcs, cycle_reps)
         good_arcs.extend(gen_arc_by_tail_rep[cycle_rep] for cycle_rep in cycle_reps)
-        cycle_rep_set = set(cycle_reps)
-        cycle_rep = cycle_rep_set.pop()
+        cycle_rep_set = kind_of_sort_by_weights(arcs,cycle_reps)
+        cycle_rep = cycle_rep_set.pop(0)
         quotient_map = {node: cycle_rep if node_rep in cycle_rep_set else node_rep for node, node_rep in quotient_map.items()}
+
+def kind_of_sort_by_weights(arcs, cycle):
+    max = get_weight(arcs, cycle[0], cycle[1])
+    index = 0
+    for i in range(len(cycle)-1):
+        if(get_weight(arcs, cycle[i], cycle[i+1]) > max):
+            index = i
+            max = get_weight(arcs, cycle[i], cycle[i+1])
+    if get_weight(arcs,cycle[-1],cycle[0]) > max:
+        index = -1
+    cycle[0],cycle[index] = cycle[index],cycle[0]
+    return cycle
+
+def get_weight(arcs, tail, head):
+    for arc in arcs:
+        if(arc.tail == tail and arc.head == head):
+            return arc.weight
 
 def find_cycle(successor, sink):
     visited = {sink}
@@ -70,7 +88,7 @@ if __name__ == "__main__":
     negative_arcs = [Arc(1, -17, 0), Arc(2, -16, 0), Arc(3, -19, 0), Arc(4, -16, 0), Arc(5, -16, 0), Arc(6, -18, 0), Arc(2, -3, 1), Arc(3, -3, 1), Arc(4, -11, 1), Arc(5, -10, 1), Arc(6, -12, 1), Arc(1, -3, 2), Arc(3, -4, 2), Arc(4, -8, 2), Arc(5, -8, 2), Arc(6, -11, 2), Arc(1, -3, 3), Arc(2, -4, 3), Arc(4, -12, 3), Arc(5, -11, 3), Arc(6, -14, 3), Arc(1, -11, 4), Arc(2, -8, 4), Arc(3, -12, 4), Arc(5, -6, 4), Arc(6, -10, 4), Arc(1, -10, 5), Arc(2, -8, 5), Arc(3, -11, 5), Arc(4, -6, 5), Arc(6, -4, 5), Arc(1, -12, 6), Arc(2, -11, 6), Arc(3, -14, 6), Arc(4, -10, 6), Arc(5, -4, 6)]
 
 
-    test_arcs = [Arc(1, 9, 0), Arc(2, 8, 0), Arc(3, 9, 0), Arc(2, 20, 1), Arc(3, 3, 1), Arc(1,30, 2), Arc(3, 30, 2), Arc(2, 50, 3), Arc(1, 11, 3)]
+    test_arcs = [Arc(1, 9, 0), Arc(2, 9, 0), Arc(3, 9, 0), Arc(2, 20, 1), Arc(3, 3, 1), Arc(1,30, 2), Arc(3, 30, 2), Arc(2, 50, 3), Arc(1, 11, 3)]
 
     sink = 0
 
